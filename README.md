03aae740b899772c2442d054a4de1e17c0334162
David Konsumer <konsumer@jetboystudio.com>
2022-09-30 17:49:21 -0700

    customize and tune build-target

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 This is a very simple raylib app built with cmake that automatically releases for all platforms.
 
-## lcoal usage
+## local usage
 
 ```
 rm -rf build
 mkdir build
 cd build
 cmake ..
+make
+./my_raylib_game
 ```
---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (450)
+
+// draw text with centered origin
+void DrawCenteredText (const char* text, int x, int y, int fontSize, Color color) {
+  const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, fontSize, 1);
+  DrawText(text, x - text_size.x / 2, y - text_size.y / 2, fontSize, color);
+}
+
+int main(void) {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Example Game");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawCenteredText("OMG! IT WORKS!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
